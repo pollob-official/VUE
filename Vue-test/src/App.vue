3aba28be05f999eed7418f291a86967f3e3af6f9
Pollob <pollob.workmail@gmail.com>
2026-01-27 12:17:33 +0600

    basic of  vue

--- Vue-test/src/App.vue
+++ Vue-test/src/App.vue
@@ -1,30 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import Counter from './components/Counter.vue';
+
+let Name= "Pollob";
+let html = "<span style='color: blue;'>Pabna</span> <span style='color: green;'>Bangladesh</span>"
+
+let active= ref(true);
+const changeName = ()=>{
+  active.value= !active.value;
+}
+
+let students= ["Rahim", "Karim", "Jabbar", "Salam"];
 </script>
 
+
+
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<h1>Hello!   <span v-text="Name"></span></h1>
+<h2>Address: <span v-html="html"></span></h2>
+    <Counter/>  
+<h1 v-if="active">Pollob Ahmed Sagor</h1>
+<h2 v-else>Full Stack Web Developer</h2>
+<button style=" background: red;" v-on:click="changeName">ChangeName</button>
+<ol>
+    <li v-for="(student, i) in students" :key="i">{{ student }}</li>
+</ol>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
